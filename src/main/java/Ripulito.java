73d98414085c1035ff440ca971152d8c997e5a3e
Peiki <peikimo@hotmail.it>
2018-10-22 22:18:27 +0200

    Enhanced Code

--- src/main/java/Ripulito.java
+++ src/main/java/Ripulito.java
@@ -2,21 +2,24 @@ public class Ripulito {
     private int[] aa;
     private int[] x;
     private int ll;
-    public static void main(String a[]){
-        int[] y = {45,23,11,89,77,98,4,28,65,43};
+
+    public static void main(String a[]) {
+        int[] y = {45, 23, 11, 89, 77, 98, 4, 28, 65, 43};
         Ripulito mms = new Ripulito();
         mms.s(y);
-        for(int i:y){
+        for (int i : y) {
             System.out.print(i);
             System.out.print(" ");
         }
     }
+
     public void s(int xx[]) {
         this.aa = xx;
         this.ll = xx.length;
         this.x = new int[ll];
         foo(0, ll - 1);
     }
+
     private void foo(int i, int j) {
         if (i < j) {
             int k = i + (j - i) / 2;
@@ -25,6 +28,7 @@ public class Ripulito {
             mp(i, k, j);
         }
     }
+
     private void mp(int jj, int ii, int kk) {
         for (int i = jj; i <= kk; i++) {
             x[i] = aa[i];
